efff6893e4b53087e5eb2819c14ca5939bd2c867
Tarjei Skorgenes <tas@spk.no>
2015-06-09 19:36:55 +0200

    SPKMASTER-11291, POPKORN-156: Oppgraderer til assertj-core 3.0.0 for å få bedre støtte for java 8 + assertThatThrownB
    Endringar er avhengig av ein fiks på domenemodellen for å la eksterne modular asserte ppremiesatsane utanfrå. Dette er påkrevd fordi samanlikning av prosentar ikkje er støtta via equals-metoda til Prosent-klassa på grunn av behov for å angi presisjonsnivå på samanlikninga.

--- src/test/java/no/spk/pensjon/faktura/tidsserie/storage/csv/AvtaleproduktOversetterTest.java
+++ src/test/java/no/spk/pensjon/faktura/tidsserie/storage/csv/AvtaleproduktOversetterTest.java
@@ -1,6 +1,12 @@
 package no.spk.pensjon.faktura.tidsserie.storage.csv;
 
 import static java.util.Arrays.asList;
+import static java.util.Optional.of;
+import static no.spk.pensjon.faktura.tidsserie.domain.avtaledata.Assertions.assertAdministrasjonsgebyrbeloep;
+import static no.spk.pensjon.faktura.tidsserie.domain.avtaledata.Assertions.assertArbeidsgiverbeloep;
+import static no.spk.pensjon.faktura.tidsserie.domain.avtaledata.Assertions.assertMedlemsbeloep;
+import static no.spk.pensjon.faktura.tidsserie.domain.grunnlagsdata.AvtaleId.valueOf;
+import static no.spk.pensjon.faktura.tidsserie.domain.grunnlagsdata.Kroner.kroner;
 import static no.spk.pensjon.faktura.tidsserie.domain.grunnlagsdata.Prosent.prosent;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -10,7 +16,6 @@ import java.util.Optional;
 
 import no.spk.pensjon.faktura.tidsserie.domain.avtaledata.Avtaleprodukt;
 import no.spk.pensjon.faktura.tidsserie.domain.avtaledata.Produktinfo;
-import no.spk.pensjon.faktura.tidsserie.domain.grunnlagsdata.AvtaleId;
 import no.spk.pensjon.faktura.tidsserie.domain.grunnlagsdata.Kroner;
 import no.spk.pensjon.faktura.tidsserie.domain.grunnlagsdata.Produkt;
 import no.spk.pensjon.faktura.tidsserie.domain.grunnlagsdata.Prosent;
@@ -26,19 +31,27 @@ import org.junit.rules.ExpectedException;
 public class AvtaleproduktOversetterTest {
     @Rule
     public final ExpectedException e = ExpectedException.none();
-    
+
     private final AvtaleproduktOversetter oversetter = new AvtaleproduktOversetter();
 
     @Test
     public void testSolskinnslinjeProsent() throws Exception {
-        assertSolskinnslinje("AVTALEPRODUKT;100001;PEN;2007.01.01;2010.08.31;11;0.00;0.00;10.00;0;0.0;0.00",
-                new Satser<>(Prosent.ZERO, Prosent.ZERO, prosent("10%")));
+        final Avtaleprodukt resultat =
+                oversett("AVTALEPRODUKT;100001;PEN;2007.01.01;2010.08.31;11;0.00;0.00;10.00;0;0.0;0.00");
+
+        assertArbeidsgiverbeloep(resultat).isEqualTo(of("0%"));
+        assertMedlemsbeloep(resultat).isEqualTo(of("0%"));
+        assertAdministrasjonsgebyrbeloep(resultat).isEqualTo(of("10%"));
     }
 
     @Test
     public void testSolskinnslinjeKroner() throws Exception {
-        assertSolskinnslinje("AVTALEPRODUKT;100001;PEN;2007.01.01;2010.08.31;11;0.00;0.00;00.00;0;0.0;100.00",
-                new Satser<>(Kroner.ZERO, Kroner.ZERO, Kroner.kroner(100)));
+        final Avtaleprodukt resultat =
+                oversett("AVTALEPRODUKT;100001;PEN;2007.01.01;2010.08.31;11;0.00;0.00;00.00;0;0.0;100.00");
+
+        assertArbeidsgiverbeloep(resultat).isEqualTo(of("kr 0"));
+        assertMedlemsbeloep(resultat).isEqualTo(of("kr 0"));
+        assertAdministrasjonsgebyrbeloep(resultat).isEqualTo(of("kr 100"));
     }
 
     @Test
@@ -50,24 +63,17 @@ public class AvtaleproduktOversetterTest {
     @Test
     public void testUkjentProduktstrengSkalGiUkjentProduktEnum() throws Exception {
         assertThat(
-                oversetter.oversett(
-                        asList(
-                                "AVTALEPRODUKT;100001;XXX;2007.01.01;2010.08.31;11;0.00;0.00;10.00;0;0;0".split(";")
-                        )
-                ).produkt()
+                oversett("AVTALEPRODUKT;100001;XXX;2007.01.01;2010.08.31;11;0.00;0.00;10.00;0;0;0").produkt()
         ).isEqualTo(Produkt.UKJ);
     }
 
     public void assertSolskinnslinje(String csvStreng, Satser<?> satser) throws Exception {
         assertThat(
-                oversetter.oversett(
-                        asList(csvStreng.split(";")
-                        )
-                )
+                oversett(csvStreng)
         ).isEqualToComparingFieldByField(new Avtaleprodukt(
                 LocalDate.of(2007, 1, 1),
-                Optional.of(LocalDate.of(2010, 8, 31)),
-                AvtaleId.valueOf("100001"),
+                of(LocalDate.of(2010, 8, 31)),
+                valueOf("100001"),
                 Produkt.PEN,
                 new Produktinfo(11),
                 satser));
@@ -77,11 +83,7 @@ public class AvtaleproduktOversetterTest {
     public void testProduktlinjeMedPremisatserOgKronestatserKasterFeilmelding() throws Exception {
         e.expect(IllegalStateException.class);
         e.expectMessage("Både prosentsatser og kronesatser kan ikke være i bruk for et avtaleprodukt.");
-        oversetter.oversett(
-                asList(
-                        "AVTALEPRODUKT;100001;XXX;2007.01.01;2010.08.31;11;0.00;0.00;10.00;0;0;2".split(";")
-                )
-        );
+        oversett("AVTALEPRODUKT;100001;XXX;2007.01.01;2010.08.31;11;0.00;0.00;10.00;0;0;2");
     }
 
     @Test
@@ -92,5 +94,12 @@ public class AvtaleproduktOversetterTest {
                 "arbeidsgiverpremie beløp, medlemspremie beløp, administrasjongebyr beløp");
         oversetter.oversett(new ArrayList<>());
         e.expectMessage("Både prosentsatser og kronesatser kan ikke være i bruk for et avtaleprodukt.");
+    }
+
+    private Avtaleprodukt oversett(final String text) {
+        return oversetter.oversett(
+                asList(text.split(";")
+                )
+        );
     }
 }
