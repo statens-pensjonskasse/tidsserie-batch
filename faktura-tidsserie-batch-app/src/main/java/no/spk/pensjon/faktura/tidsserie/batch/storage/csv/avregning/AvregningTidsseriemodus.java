d1023f903e6ca25c36114a826dcc28608717aa36
Tarjei Skorgenes <tas@spk.no>
2015-11-02 08:43:18 +0100

    SPKMASTER-12657, POPKORN-197: Legger til støtte for en ny modus, avregning_tidsserie. Den nye modusen genererer tidsseriar på periodenivå og er basert på ein modifisert kopi av live_tidsserien.
    Den nye modusen fungerer identisk med live-tidsserien fram til serialiseringa, det blir generert opp eit observasjonsunderlag for kvar måned i observasjonsperioda (som implisitt er kobla til avregningsperioda i den forstand at begge to må ha lik lengde sjølv om det ikkje blir handheva på noko vis). Alle observasjonsunderlaga unntatt dei for 31. desember, blir filtrert bort. Dette har ein viss ytelsesmessig effekt men den er lav nok til at vi gir bæng i at vi genererer opp 12 gangar så mange underlag som vi strengt tatt behøver.

---
+++ faktura-tidsserie-batch-app/src/main/java/no/spk/pensjon/faktura/tidsserie/batch/storage/csv/avregning/AvregningTidsseriemodus.java
@@ -0,0 +1,144 @@
+package no.spk.pensjon.faktura.tidsserie.batch.storage.csv.avregning;
+
+import static java.time.LocalDate.now;
+import static java.util.stream.Collectors.joining;
+
+import java.nio.file.Path;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+import no.spk.pensjon.faktura.tidsserie.batch.CSVFormat;
+import no.spk.pensjon.faktura.tidsserie.batch.StorageBackend;
+import no.spk.pensjon.faktura.tidsserie.batch.TidsperiodeFactory;
+import no.spk.pensjon.faktura.tidsserie.batch.Tidsseriemodus;
+import no.spk.pensjon.faktura.tidsserie.batch.Tidsserienummer;
+import no.spk.pensjon.faktura.tidsserie.domain.avregning.Avregningsperiode;
+import no.spk.pensjon.faktura.tidsserie.domain.reglar.AvregningsRegelsett;
+import no.spk.pensjon.faktura.tidsserie.domain.reglar.Regelsett;
+import no.spk.pensjon.faktura.tidsserie.domain.tidsperiode.Tidsperiode;
+import no.spk.pensjon.faktura.tidsserie.domain.tidsserie.Observasjonsdato;
+import no.spk.pensjon.faktura.tidsserie.domain.tidsserie.Observasjonspublikator;
+import no.spk.pensjon.faktura.tidsserie.domain.tidsserie.TidsserieFacade;
+import no.spk.pensjon.faktura.tidsserie.domain.underlag.Underlag;
+import no.spk.pensjon.faktura.tidsserie.domain.underlag.Underlagsperiode;
+import no.spk.pensjon.faktura.tidsserie.storage.GrunnlagsdataRepository;
+import no.spk.pensjon.faktura.tidsserie.storage.csv.AvregningsperiodeOversetter;
+import no.spk.pensjon.faktura.tidsserie.storage.csv.CSVInput;
+
+/**
+ * {@link AvregningTidsseriemodus} kan benyttes for å generere en tidsserie tilrettelagt for avregning.
+ * <br>
+ * Modusen genererer tidsserier som lagres til flate filer på CSV-format, i henhold til formatspesifikasjonen
+ * definert via {@link Avregningformat}.
+ * <br>
+ * Foreløpig støtter avregningsmodusen kun premieberegning for pensjonsproduktene, forsikringsproduktene avventer
+ * avklaring fra forretning på hvordan de skal avregnes.
+ *
+ * @author Tarjei Skorgenes
+ * @see Avregningformat
+ * @since 1.2.0
+ */
+public class AvregningTidsseriemodus implements Tidsseriemodus {
+    private final CSVFormat outputFormat = new Avregningformat();
+
+    private final Regelsett reglar = new AvregningsRegelsett();
+
+    private final Tidsserienummer nummer = Tidsserienummer.genererForDato(now());
+
+    @Override
+    public Stream<Tidsperiode<?>> referansedata(final TidsperiodeFactory perioder) {
+        return Stream.of(
+                reglar.reglar(),
+                perioder.loennsdata(),
+                perioder.perioderAvType(Avregningsperiode.class)
+        )
+                .flatMap(s -> s);
+    }
+
+    @Override
+    public GrunnlagsdataRepository repository(final Path directory) {
+        return new CSVInput(directory)
+                .addOversettere(new AvregningsperiodeOversetter());
+    }
+
+    /**
+     * Navnet til alle kolonnene som CSV-filene kan inneholde verdier for.
+     *
+     * @return ein strøm med navn på alle kolonnene i CSV-filene til tidsserien
+     * @see Avregningformat#kolonnenavn()
+     */
+    @Override
+    public Stream<String> kolonnenavn() {
+        return outputFormat.kolonnenavn();
+    }
+
+    /**
+     * Beregningsreglene som benyttes ved generering av tidsserien.
+     *
+     * @return regelsettet som modusen benytter
+     * @see AvregningsRegelsett
+     */
+    @Override
+    public Regelsett regelsett() {
+        return reglar;
+    }
+
+    /**
+     * Genererer ein ny publikator som genererer og lagrar ein observasjon pr underlagsperiode pr
+     * observasjonsunderlag.
+     * <br>
+     *
+     * @param facade      fasada som blir brukt for å generere tidsserien
+     * @param serienummer serienummer som alle eventar som blir sendt vidare til <code>backend</code> for persistering
+     *                    skal tilhøyre
+     * @param publikator  backend-systemet som observasjonane av kvar periode blir lagra via
+     * @return ein by publikator som serialiserer og lagrar alle underlagsperioder for kvart observasjonsunderlag i
+     * tidsserien som blir generert av <code>facade</code>
+     * @see Avregningformat#serialiser(Underlag, Underlagsperiode)
+     * @see StorageBackend#lagre(Consumer)
+     */
+    @Override
+    public Observasjonspublikator create(final TidsserieFacade facade, long serienummer, final StorageBackend publikator) {
+        return nyPublikator(
+                this::serialiserPeriode,
+                line -> lagre(publikator, line, serienummer)
+        );
+    }
+
+    /**
+     * Genererer ein ny publikator som ved hjelp av <code>mapper </code> serialiserer alle periodene frå alle
+     * observasjonsunderlag, til ei form som deretter blir lagra via <code>lagring</code>.
+     *
+     * @param mapper  serialiserer observasjonsunderlagas underlagsperioder til formatet <code>lagring</code> skal lagre på
+     * @param lagring tar den serialiserte versjonen av underlagsperiodene og lagrar dei
+     * @param <T>     datatypen underlagsperiodene blir serialisert til
+     * @return ein ny observasjonspublikator som kan brukast til å serialisere og lagre innholdet frå ein tidsserie
+     */
+    <T> Observasjonspublikator nyPublikator(
+            final Function<Underlag, Stream<T>> mapper, final Consumer<T> lagring) {
+        return s -> s
+                .peek(this::annoterMedTidsserienummer)
+                .filter(u -> u.annotasjonFor(Observasjonsdato.class).erAaretsSisteDag())
+                .flatMap(mapper)
+                .forEach(lagring);
+    }
+
+    void lagre(final StorageBackend publikator, final String line, final long serienummer) {
+        publikator.lagre(event -> event.serienummer(serienummer).buffer.append(line).append("\n"));
+    }
+
+    private Underlag annoterMedTidsserienummer(Underlag u) {
+        return u.annoter(Tidsserienummer.class, nummer);
+    }
+
+    private Stream<String> serialiserPeriode(final Underlag observasjonsunderlag) {
+        return observasjonsunderlag
+                .stream()
+                .map(underlagsperiode -> outputFormat.serialiser(
+                        observasjonsunderlag,
+                        underlagsperiode
+                ))
+                .map(kolonneVerdiar -> kolonneVerdiar.map(Object::toString).collect(joining(";")));
+    }
+}
---
+++ faktura-tidsserie-batch-app/src/test/java/no/spk/pensjon/faktura/tidsserie/batch/storage/csv/avregning/AvregningformatTest.java
@@ -0,0 +1,150 @@
+package no.spk.pensjon.faktura.tidsserie.batch.storage.csv.avregning;
+
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Stream.of;
+import static no.spk.pensjon.faktura.tidsserie.Datoar.dato;
+import static no.spk.pensjon.faktura.tidsserie.domain.avregning.Avregningsversjon.avregningsversjon;
+import static no.spk.pensjon.faktura.tidsserie.domain.grunnlagsdata.AvtaleId.avtaleId;
+import static no.spk.pensjon.faktura.tidsserie.domain.grunnlagsdata.Foedselsdato.foedselsdato;
+import static no.spk.pensjon.faktura.tidsserie.domain.grunnlagsdata.Personnummer.personnummer;
+import static no.spk.pensjon.faktura.tidsserie.domain.grunnlagsdata.StillingsforholdId.stillingsforhold;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.stream.Stream;
+
+import no.spk.pensjon.faktura.tidsserie.domain.avregning.Avregningsversjon;
+import no.spk.pensjon.faktura.tidsserie.domain.grunnlagsdata.AvtaleId;
+import no.spk.pensjon.faktura.tidsserie.domain.grunnlagsdata.Foedselsnummer;
+import no.spk.pensjon.faktura.tidsserie.domain.grunnlagsdata.StillingsforholdId;
+import no.spk.pensjon.faktura.tidsserie.domain.tidsserie.Observasjonsdato;
+import no.spk.pensjon.faktura.tidsserie.domain.underlag.PaakrevdAnnotasjonManglarException;
+import no.spk.pensjon.faktura.tidsserie.domain.underlag.Underlag;
+import no.spk.pensjon.faktura.tidsserie.domain.underlag.Underlagsperiode;
+import no.spk.pensjon.faktura.tidsserie.domain.underlag.UnderlagsperiodeBuilder;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+/**
+ * Enheitstestar for {@link Avregningformat}.
+ *
+ * @author Tarjei Skorgenes
+ */
+public class AvregningformatTest {
+    @Rule
+    public final ExpectedException e = ExpectedException.none();
+
+    private final Avregningformat format = new Avregningformat();
+
+    @Test
+    public void skalHaLikeAntallKolonneISerialiseringaSomIKolonnenavna() {
+        final List<String> expected = format.kolonnenavn()
+                .collect(toList());
+        final List<Object> actual = serialiser(observasjonsdato(), eiPeriodeMedKunObligatoriskeVerdiar())
+                .collect(toList());
+        assertThat(actual)
+                .as("kolonner generert av serialiseringa vs kolonnenavna serialiseringa seier den støttar")
+                .hasSameSizeAs(expected);
+    }
+
+    @Test
+    public void skal_feile_viss_observasjonsdato_manglar() {
+        forventPaakrevdAnnotasjonFeilForType(Observasjonsdato.class);
+
+        serialiser(
+                observasjonsdato(),
+                eiPeriodeMedKunObligatoriskeVerdiar(),
+                (ignorert, p) -> new Underlag(of(p))
+        );
+    }
+
+    @Test
+    public void skal_feile_viss_foedselsnummer_manglar() {
+        forventPaakrevdAnnotasjonFeilForType(Foedselsnummer.class);
+
+        serialiser(
+                observasjonsdato(),
+                eiPeriodeMedKunObligatoriskeVerdiar()
+                        .uten(Foedselsnummer.class)
+        );
+    }
+
+    @Test
+    public void skal_feile_viss_stillingsforhold_manglar() {
+        forventPaakrevdAnnotasjonFeilForType(StillingsforholdId.class);
+
+        serialiser(
+                observasjonsdato(),
+                eiPeriodeMedKunObligatoriskeVerdiar()
+                        .uten(StillingsforholdId.class)
+        );
+    }
+
+    @Test
+    public void skal_feile_viss_avtale_manglar() {
+        forventPaakrevdAnnotasjonFeilForType(AvtaleId.class);
+
+        serialiser(
+                observasjonsdato(),
+                eiPeriodeMedKunObligatoriskeVerdiar()
+                        .uten(AvtaleId.class)
+        );
+    }
+
+    @Test
+    public void skal_feile_viss_avregningsversjon_manglar() {
+        forventPaakrevdAnnotasjonFeilForType(Avregningsversjon.class);
+
+        serialiser(
+                observasjonsdato(),
+                eiPeriodeMedKunObligatoriskeVerdiar()
+                        .uten(Avregningsversjon.class)
+        );
+    }
+
+    private void forventPaakrevdAnnotasjonFeilForType(final Class<?> clazz) {
+        e.expect(PaakrevdAnnotasjonManglarException.class);
+        e.expectMessage(clazz.getSimpleName());
+    }
+
+    private Observasjonsdato observasjonsdato() {
+        return new Observasjonsdato(dato("2000.01.31"));
+    }
+
+    private Stream<Object> serialiser(final Observasjonsdato dato, final UnderlagsperiodeBuilder builder) {
+        return serialiser(dato, builder, this::underlag);
+    }
+
+    private Stream<Object> serialiser(final Observasjonsdato dato, final UnderlagsperiodeBuilder builder,
+                                      final BiFunction<Observasjonsdato, Underlagsperiode, Underlag> underlag) {
+        final Underlagsperiode periode = builder.bygg();
+        return format.serialiser(
+                underlag.apply(dato, periode),
+                periode
+        )
+                .collect(toList())
+                .stream();
+    }
+
+    private Underlag underlag(final Observasjonsdato dato, final Underlagsperiode periode) {
+        return new Underlag(of(periode))
+                .annoter(Observasjonsdato.class, dato);
+    }
+
+    private UnderlagsperiodeBuilder eiPeriodeMedKunObligatoriskeVerdiar() {
+        return new UnderlagsperiodeBuilder()
+                .fraOgMed(dato("2000.01.01"))
+                .tilOgMed(dato("2000.01.31"))
+                .med(new Foedselsnummer(
+                        foedselsdato(19800101),
+                        personnummer(1)
+                ))
+                .med(stillingsforhold(1L))
+                .med(avtaleId(223344L))
+                .med(avregningsversjon(29))
+                ;
+    }
+}
