f71b550abe1f309252a2d77b0558bc93965ffca5
Tarjei Skorgenes <tas@spk.no>
2016-01-22 08:52:19 +0100

    SPKMASTER-12773, POPKORN-232: Fjernar kobling frå live_tidsseriens testar til andre testklasser i batch-modulen slik at dei skal kunne flyttast ut av app-modulen.

--- faktura-tidsserie-batch-app/src/test/java/no/spk/pensjon/faktura/tidsserie/plugin/modus/underlagsperioder/LiveTidsseriemodusTest.java
+++ faktura-tidsserie-batch-app/src/test/java/no/spk/pensjon/faktura/tidsserie/plugin/modus/underlagsperioder/LiveTidsseriemodusTest.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
-import no.spk.pensjon.faktura.tidsserie.batch.ServiceRegistryRule;
 import no.spk.pensjon.faktura.tidsserie.batch.core.AgentInitializer;
 import no.spk.pensjon.faktura.tidsserie.batch.core.Katalog;
 import no.spk.pensjon.faktura.tidsserie.batch.core.StorageBackend;
