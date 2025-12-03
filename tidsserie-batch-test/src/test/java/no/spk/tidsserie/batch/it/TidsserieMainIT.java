e45af5fad3dcee93ecf7d6e561f16e80ff954d90
Isak Bjørn <isak.eriksen.bjorn@spk.no>
2025-12-03 14:41:45 +0100

    chore: bumper failsafe og endrer cucumber selector

--- tidsserie-batch-test/src/test/java/no/spk/tidsserie/batch/it/TidsserieMainIT.java
+++ tidsserie-batch-test/src/test/java/no/spk/tidsserie/batch/it/TidsserieMainIT.java
@@ -5,12 +5,13 @@ import static io.cucumber.core.options.Constants.FILTER_TAGS_PROPERTY_NAME;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 
 @Suite
 @IncludeEngines("cucumber")
 @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "not @ignore")
-@SelectClasspathResource("no/spk/tidsserie/batch/it")
+@SelectPackages("no.spk.tidsserie.batch.it")
 public class TidsserieMainIT {
 }
